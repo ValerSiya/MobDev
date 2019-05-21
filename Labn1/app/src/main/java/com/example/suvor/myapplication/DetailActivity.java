637e8064018846bcefdbef0433e5f6147f8511c5
ValerSiya <valeriesuvorova88@gmail.com>
2019-05-21 20:19:03 +0300

    Add files via upload

---
+++ Labn1/app/src/main/java/com/example/suvor/myapplication/DetailActivity.java
@@ -0,0 +1,24 @@
+package com.example.suvor.myapplication;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity  extends AppCompatActivity {
+    public static final String EXTRA_URL = "url";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
+        setContentView(R.layout.activity_detail);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String url = extras.getString(EXTRA_URL);
+            DetailFragment detailFragment = (DetailFragment) getFragmentManager()
+                    .findFragmentById(R.id.detailFragment);
+            detailFragment.setText(url);
+        }
+    }
+}
---
+++ Labn1/app/src/main/java/com/example/suvor/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.suvor.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+        implements ListFragment.OnFragmentInteractionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onFragmentInteraction(String link) {
+        DetailFragment fragment = (DetailFragment) getFragmentManager()
+                .findFragmentById(R.id.detailFragment);
+        if (fragment != null && fragment.isInLayout()) {
+            fragment.setText(link);
+        } else {
+            Intent intent = new Intent(getApplicationContext(),
+                    DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_URL, link);
+            startActivity(intent);
+        }
+    }
+}
